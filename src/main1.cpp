c9fd1533b10dfaa0b459f92c9648afe456b63d97
Ivanov-Andrey-Romanovich <32748077+Ivanov-Andrey-Romanovich@users.noreply.github.com>
2019-12-10 18:59:07 +0300

    update

--- src/main1.cpp
+++ src/main1.cpp
@@ -3,9 +3,9 @@
 
 int main() {
 	const size_t n = 6;
-	int* val = new int[n] {5, 3, 2, 4, 1, 6};
+	//int* val = new int[n] {5, 3, 2, 4, 1, 6};
 	//double val[n]{ 4.0, 3.0, 2.4, 2.5, 2.1, -1 };
-	//char* val[n]{ (char*)"ffff", (char*)"fff2", (char*)"f", (char*)"ff", (char*)"vvv", (char*)"fffrr" };
+	char* val[n]{ (char*)"ffff", (char*)"fff2", (char*)"f", (char*)"ff", (char*)"vvv", (char*)"fffrr" };
 	msort(val, n);
 	for (int i = 0; i < 6; i++)
 		std::cout << val[i] << " ";
