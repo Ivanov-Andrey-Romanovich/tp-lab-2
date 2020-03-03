eb2aba6f24180b2f4acadfb2c786cd53049096da
Ivanov-Andrey-Romanovich <32748077+Ivanov-Andrey-Romanovich@users.noreply.github.com>
2020-03-03 22:25:32 +0300

    Update task2.h

--- include/task2.h
+++ include/task2.h
@@ -1,22 +1,22 @@
 #pragma once
 template<class T>
-//T gen()
-//{
-//	static int t = 48;
-//	return t++;
-//}
+T gen()
+{
+	static int t = 48;
+	return t++;
+}
 
-//template<>
-//char* gen()
-//{
-//	static unsigned t = 0;
-//	t++;
-//	char* s = new char[t + 1];
-//	for (size_t i = 0; i < t; i++)
-//		s[i] = '0' + i;
-//	s[t] = '\0';
-//	return s;
-//}
+template<>
+char* gen()
+{
+	static unsigned t = 0;
+	t++;
+	char* s = new char[t + 1];
+	for (size_t i = 0; i < t; i++)
+		s[i] = '0' + i;
+	s[t] = '\0';
+	return s;
+}
 template <typename T, int n> T* createArr(T(*gen)()) {
 	T* arr = new T[n];
 	for (int i = 0; i < n; i++) {
