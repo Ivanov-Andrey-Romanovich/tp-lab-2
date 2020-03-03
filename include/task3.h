7648bb92fd8af1bdcdec3d29a3f63061cd846251
Ivanov-Andrey-Romanovich <32748077+Ivanov-Andrey-Romanovich@users.noreply.github.com>
2020-03-03 22:28:33 +0300

    Update task3.h

--- include/task3.h
+++ include/task3.h
@@ -1,19 +1,19 @@
 #pragma once
 #include <cstring>
-template<class T>
-T change(T val)
-{
-	return val + 1;
-}
+//template<class T>
+//T change(T val)
+//{
+//	return val + 1;
+//}
 
-template<>
-char* change(char* val)
-{
-	size_t n = strlen(val);
-	for (size_t i = 0; i < n; i++)
-		val[i] = val[i] + 1;
-	return val;
-}
+//template<>
+//char* change(char* val)
+//{
+//	size_t n = strlen(val);
+//	for (size_t i = 0; i < n; i++)
+//		val[i] = val[i] + 1;
+//	return val;
+//}
 
 template <typename T, int n> void map(T* arr, T(*change)(T val)) {
 	for (int i = 0; i < n; i++) {
